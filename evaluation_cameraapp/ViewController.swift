e2c1457c4d1baedbe896c96dd2e08d176c8e14d6
milancho13 <milan.cho61@gmail.com>
2021-03-21 16:45:44 +0900

    error?

--- evaluation_cameraapp/ViewController.swift
+++ evaluation_cameraapp/ViewController.swift
@@ -19,6 +19,10 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     @IBAction func cameraLaunchAction(_ sender: Any) {
         if UIImagePickerController.isSourceTypeAvailable(.camera) {
             print("Camera can be used")
+            let ipc = UIImagePickerController()
+            ipc.sourceType = .camera
+            ipc.delegate = self
+            present(ipc, animated:true, completion: nil)
         } else {
             print("No")
         }
@@ -27,5 +31,10 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     @IBAction func shareAction(_ sender: Any) {
     }
     
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info:
+        [String : Any]) {
+        photoImage.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        dismiss(animated: true, completion: nil)
+    }
 }
 
